9fe76b0bfd1f8415875040d3d0ce4dfa459c0fa3
Daniel Kennedy <djk2120@columbia.edu>
2024-10-25 11:59:55 -0600

    old sparsegrid still looks decent

---
+++ sparsegrid/utils.py
@@ -0,0 +1,8 @@
+def gmean(da,la):
+    if 'gridcell' in da.dims:
+        dim='gridcell'
+    else:
+        dim=['lat','lon']
+    x=(da*la).sum(dim=dim)/la.sum()
+    x.attrs=da.attrs
+    return x.compute()
