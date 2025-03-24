a7f30917a93782cb797dbbec3c0305d76315d84f
Daniel Kennedy <djk2120@columbia.edu>
2025-03-24 14:14:49 -0600

    adding logic for failed sims

--- postprocessing/postp_lhc_basic.py
+++ postprocessing/postp_lhc_basic.py
@@ -25,17 +25,26 @@ def pp(ds):
 # load and process data
 
 f=sorted(glob.glob(dir+'*'+lhc+'*.'+tape+'.*'))
+if len(f)==0:
+    #hacky way to generate correctly shaped nan output for failed simulations
+    bad=True
+    f=sorted(glob.glob(dir+'*lhc0000*.'+tape+'.*')) 
+    
+
 ds=pp(xr.open_dataset(f[-2]))
 
 # calculate global annual mean
 la=xr.open_dataset(utils_path+'landarea_retrain_h0.nc').landarea
 out=xr.Dataset()
-    
 for v in dvs:
     x=amean(ds[v])
     out[v+'_global_amean']=gmean(x,la)
     out[v+'_global_amean'].attrs=ds[v].attrs
-    
+
+# nan output if no files
+if bad:
+    out=np.nan*out
+
 # save 
 fout=out_dir+f[0].split('/')[-1].split('clm2')[0]+'postp.nc'
 out.to_netcdf(fout)
