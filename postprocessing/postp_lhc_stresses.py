3a3d4ead4ace69c308c93fa5fd9535f35b211bf0
Daniel Kennedy <djk2120@columbia.edu>
2025-04-24 17:43:09 -0600

    update to also calc biome

--- postprocessing/postp_lhc_stresses.py
+++ postprocessing/postp_lhc_stresses.py
@@ -6,73 +6,109 @@ sys.path.append('/glade/u/home/linnia/ctsm6_ppe/')
 from utils.pyfunctions import *
 utils_path = '/glade/u/home/linnia/ctsm6_ppe/utils/'
 
-lhc=sys.argv[1] 
-out_dir=sys.argv[2]
+lhc=sys.argv[1]
+in_dir=sys.argv[2]
+out_dir=sys.argv[3]
+
+if in_dir[-1]!='/':
+    in_dir+='/'
 if out_dir[-1]!='/':
     out_dir+='/'
 ######################################################
-# setup 
-dir='/glade/campaign/cgd/tss/projects/PPE/ctsm6_lhc/hist/'
-tape='h0'
-dvs=['GPP','NPP','NPP_NUPTAKE','TLAI','BTRANMN']
-
+# setup
+dvs=['NPP','GPP','AR','NPP_NUPTAKE','TLAI','BTRANMN','Vcmx25Z']
 def pp(ds):
     yr0=str(ds['time.year'][0].values)
     nt=len(ds.time)
     ds['time'] = xr.cftime_range(yr0,periods=nt,freq='MS',calendar='noleap') #fix time bug
     return ds[dvs]
-
+def bavg(da,la):
+    if 'year' not in da.dims:
+        raise ValueError('bavg meant for annualized data')
+    x=(la*da).sum(dim=['pft','vegtype'])/la.sum(dim=['pft','vegtype'])
+    return x
 ######################################################
-# find files
-f0=sorted(glob.glob(dir+'*lhc0000*.'+tape+'.*'))[-5:]
-f=sorted(glob.glob(dir+'*'+lhc+'*.'+tape+'.*'))[-5:]
-if len(f)<len(f0):
-    #hacky way to generate correctly shaped nan output for failed simulations
-    # if the requested lhc is missing, we will reanalyze lhc0000 and multiply by np.nan
-    bad=True
-    f=f0
-else:
-    bad=False
+# find files for biome and global analysis
+files={}
+for tape in ['h0','h1']:
+    keys=[f.split('transient')[1].split('.')[0][1:] for f in sorted(glob.glob(in_dir+'*'+tape+'.2020-*'))]
+    k0=keys[0]
+    f0=sorted(glob.glob(in_dir+'*'+k0 +'*.'+tape+'.*'))
+    f =sorted(glob.glob(in_dir+'*'+lhc+'*.'+tape+'.*'))
+    if len(f)<len(f0):
+        #hacky way to generate correctly shaped nan output for failed simulations
+        # if the requested lhc is missing, we will reanalyze lhc0000 and multiply by np.nan
+        bad=True
+        f=f0
+    else:
+        bad=False
+    files[tape]=f
 fout=out_dir+f[0].split('/')[-1].split('clm2')[0]+'postp.nc'
 
-#load data
-ds=xr.open_mfdataset(f,combine='by_coords',preprocess=pp)
+#load h1 data, analyzing 2000-2023
+ds=xr.open_mfdataset(files['h1'][-5:],combine='by_coords',preprocess=pp)
+f=utils_path+'lapxb_sg_sudoku_ctsm53017.nc'
+tmp=xr.open_dataset(f)
+la=tmp.lapxb_sg
 
-# calculate various stress metrics
-la=xr.open_dataset(utils_path+'landarea_retrain_h0.nc').landarea
-out=xr.Dataset()
-# photosynthetic capacity
-cf=24*60*60*365*la.sum()*1e-9
-gpp=cf*amean(ds.GPP)
-maxgpp=gmean(gpp.max(dim='year'),la)
-# respiration and nitrogen acquisition
-nppg=gmean(cf*amean(ds.NPP).mean(dim='year'),la)
-cue=nppg/gmean(gpp.mean(dim='year'),la)
-# nitrogen costs
-nppnupg=gmean(cf*amean(ds.NPP_NUPTAKE).mean(dim='year'),la)
-ncost=nppnupg/(nppnupg+nppg)
-# water stress
-btran=gmean(amean(ds.BTRANMN).mean(dim='year'),la)
-# mortality and allocation
-lai=gmean(amean(ds.TLAI).mean(dim='year'),la)
+#precursors
+gppg=bavg(amean(ds.GPP),la).mean(dim='year')
+nppnupg=bavg(amean(ds.NPP_NUPTAKE),la).mean(dim='year')
+ar=bavg(amean(ds.AR),la).mean(dim='year')
+arwonit=ar-nppnupg
+lai=bavg(amean(ds.TLAI),la).mean(dim='year')
+nppg=bavg(amean(ds.NPP),la).mean(dim='year')
+
+#biome-level metrics
+vcmx25z=bavg(amean(ds.Vcmx25Z),la).mean(dim='year')
+ncost=nppnupg/gppg
+rcost=arwonit/gppg
+wstress=bavg(amean(ds.GPP*(1-ds.BTRANMN)),la).mean(dim='year')/gppg
 alloc=lai/nppg
+cue=nppg/gppg
+
+#load h0 data, analyzing 2000-2023
+ds=xr.open_mfdataset(files['h0'][-5:],combine='by_coords',preprocess=pp)
+la=xr.open_dataset(utils_path+'landarea_retrain_h0.nc').landarea
+
+#precursors
+gppg=gmean(amean(ds.GPP),la).mean(dim='year')
+nppnupg=gmean(amean(ds.NPP_NUPTAKE),la).mean(dim='year')
+ar=gmean(amean(ds.AR),la).mean(dim='year')
+arwonit=ar-nppnupg
+lai=gmean(amean(ds.TLAI),la).mean(dim='year')
+nppg=gmean(amean(ds.NPP),la).mean(dim='year')
+
+#global metrics
+vcmx25zg=gmean(amean(ds.Vcmx25Z),la).mean(dim='year')
+ncostg=nppnupg/gppg
+rcostg=arwonit/gppg
+wstressg=gmean(amean(ds.GPP*(1-ds.BTRANMN)),la).mean(dim='year')/gppg
+allocg=lai/nppg
+cueg=nppg/gppg
+
+# concatenate and save to outbound dataset
+out=xr.Dataset()
+gvs=[vcmx25zg,ncostg,rcostg,wstressg,allocg,cueg]
+bvs=[vcmx25z,ncost,rcost,wstress,alloc,cue]
+dvs=['vcmx25z','ncost','rcost','wstress','alloc','cue']
+for bv,gv,dv in zip(bvs,gvs,dvs):
+    out[dv]=xr.concat([bv,xr.DataArray([gv],dims='biome')],dim='biome')
+
 # metadata
-maxgpp.attrs={'units':'PgC/yr','long_name':'Upper limit GPP','note':'found max at each gridcell, then globally averaged'}
-cue.attrs={'long_name':'Carbon use efficiency','units':'-','note':'NPP/GPP'}
-btran.attrs={'long_name':'BTRANMN','units':'-','note':'midday minimum'}
-alloc.attrs={'long_name':'Leaf allocation efficiency','units':'m2/m2/PgC/yr','note':'TLAI/NPP'}
-ncost.attrs={'long_name':'Carbon expenditure on nitrogen','units':'-','note':'NPP_NUPTAKE/(NPP+NPP_NUPTAKE)'}
-# save to outbound dataset
-out['maxgpp']=maxgpp
-out['cue']=cue
-out['btran']=btran
-out['alloc']=alloc
-out['ncost']=ncost
+out['vcmx25z'].attrs={'units':'umol/m2/s','long_name':'Vcmax predicted by LUNA'}
+out['wstress'].attrs={'long_name':'Water stress','units':'-','note':'GPP(1-BT) at monthly, then accumulated ann and glob, then divided by GPP'}
+out['alloc'].attrs={'long_name':'Leaf allocation efficiency','units':'m2/gC/s','note':'TLAI/NPP'}
+out['cue'].attrs={'long_name':'Carbon use efficiency','units':'-','note':'NPP/(GPP)'}
+out['ncost'].attrs={'long_name':'Carbon expenditure on nitrogen','units':'-','note':'NPP_NUPTAKE/(GPP)'}
+out['rcost'].attrs={'long_name':'Carbon expenditure on other respiration','units':'-','note':'(AR-NPP_NUPTAKE)/(GPP)'}
+out['biome_name']=[*tmp.biome_name.values,'Global']
+
 
 # nan output if no files
 if bad:
     out=np.nan*out
-    fout=fout.replace('lhc0000',lhc)
+    fout=fout.replace(k0,lhc)
     
 # save 
 out.to_netcdf(fout)
